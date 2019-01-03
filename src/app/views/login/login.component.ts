059212fae694c29c415451ceb8ac52d29a6c60f5
chriszo111 <chrisswiss@gmx.net>
2019-01-03 06:25:44 +0100

    Add LoginComponent with Facebook login

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = null;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  signInWithFacebook() {
+    this.authService.signInWithFacebook()
+    .then((res) => {
+        this.router.navigate(['/']);
+      })
+    .catch((err) => console.log(err));
+  }
+
+}
