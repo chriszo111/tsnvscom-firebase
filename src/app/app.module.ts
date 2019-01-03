4029e8f52519b0a14e315fb9f81c1ad8dac549df
chriszo111 <chrisswiss@gmx.net>
2019-01-03 06:24:39 +0100

    Add FormsModule, AuthGuard, AuthService and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth-guard.service';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -16,7 +20,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCommentAlt } from '@fortawesome/free-regular-svg-icons';
-import { faGamepad, faServer, faHeadset, faArrowDown, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faGamepad, faServer, faHeadset, faArrowDown, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faTwitch, faTeamspeak, faSteam, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { ParticlesModule } from 'angular-particle';
 import { TimeAgoPipe } from 'time-ago-pipe';
@@ -28,11 +32,13 @@ import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.co
 import { HomeComponent } from './content/home/home.component';
 import { ContactComponent } from './help/contact/contact.component';
 import { ChatComponent } from './help/chat/chat.component';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 // Add all regular icons to library
 library.add(faCommentAlt);
 // Add all solid icons to library
-library.add(faGamepad, faServer, faHeadset, faArrowDown, faExternalLinkAlt, faCommentAlt);
+library.add(faGlobe, faGamepad, faServer, faHeadset, faArrowDown, faExternalLinkAlt, faCommentAlt);
 // Add all brand icons to library
 library.add(faFacebook, faTeamspeak, faTwitch, faTwitter, faSteam);
 
@@ -46,7 +52,9 @@ library.add(faFacebook, faTeamspeak, faTwitch, faTwitter, faSteam);
     HomeComponent,
     ContactComponent,
     TimeAgoPipe,
-    ChatComponent
+    ChatComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -58,9 +66,10 @@ library.add(faFacebook, faTeamspeak, faTwitch, faTwitter, faSteam);
     NgbModule.forRoot(),
     FontAwesomeModule,
     AppRoutingModule,
-    ParticlesModule
+    ParticlesModule,
+    FormsModule
   ],
-  providers: [Title],
+  providers: [Title, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
