6ad3e972395571fa5a9de287da5bce4c030aceb6
chriszo111 <chrisswiss@gmx.net>
2019-01-03 06:25:11 +0100

    Implement first working draft of chat

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,34 +9,8 @@ import { Observable } from 'rxjs';
 })
 export class AppFooterComponent implements OnInit {
 
-  chatVisible: Boolean = false;
-  chat: any = {};
-
-  constructor() {
-    // Initialize chat
-    this.chat = {
-      visible: false,
-      button: {
-        text: 'Open Chat'
-      },
-      icon: {
-        prefix: 'far',
-        icon: 'comment-alt'
-      }
-    };
-  }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
-  }
-
-  toggleChat() {
-    this.chat.visible ? this.chat.visible = false : this.chat.visible = true;
-    if (this.chat.button.text === 'Open Chat' && this.chat.visible) {
-      this.chat.button.text = 'Close Chat';
-      this.chat.icon.prefix = 'fas';
-    } else if (this.chat.button.text === 'Close Chat' && !this.chat.visible) {
-      this.chat.button.text = 'Open Chat';
-      this.chat.icon.prefix = 'far';
-    }
   }
 }
--- src/app/help/chat/chat.component.ts
+++ src/app/help/chat/chat.component.ts
@@ -1,4 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { v1 } from 'uuid';
+import { AuthService } from 'src/app/services/auth.service';
+
+interface ChatWindow {
+  visible: boolean;
+  button: {
+    text: string;
+  };
+  icon: {
+    prefix: string;
+    name: string;
+  };
+  height: number;
+}
+
+interface ChatChannel {
+  icon: {
+    prefix: string;
+    name: string;
+  };
+  name: string;
+}
+
+interface ChatMessage {
+  authorName: string;
+  message: string;
+  timestamp: Date;
+}
+
+interface ChatMessageId extends ChatMessage {
+  id: string;
+}
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +42,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  chatVisible: Boolean = false;
+  chat: ChatWindow;
+
+  messagesCol: any;
+  messages: any;
+  channel: ChatChannel;
+  messageDoc: AngularFirestoreDocument<ChatMessage>;
+  message: Observable<ChatMessage>;
+
+  title: string;
+  text: string;
+
+  constructor(private db: AngularFirestore, private authService: AuthService) {
+    // Initialize chat
+    this.chat = {
+      visible: false,
+      button: {
+        text: 'Open Chat'
+      },
+      icon: {
+        prefix: 'far',
+        name: 'comment-alt'
+      },
+      height: window.innerHeight - 40
+    };
+
+    this.initGlobalChat();
+  }
 
   ngOnInit() {
+    this.messagesCol = this.db.collection('messages');
+
+    this.messages = this.messagesCol
+    .snapshotChanges()
+      .pipe(map(res => {
+          return res.map(r => {
+            const data = r.payload.doc.data() as ChatMessage;
+            const id = r.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  toggleChat() {
+    this.chat.visible ? this.chat.visible = false : this.chat.visible = true;
+    if (this.chat.button.text === 'Open Chat' && this.chat.visible) {
+      this.chat.button.text = 'Close Chat';
+      this.chat.icon.prefix = 'far';
+    } else if (this.chat.button.text === 'Close Chat' && !this.chat.visible) {
+      this.chat.button.text = 'Open Chat';
+      this.chat.icon.prefix = 'far';
+    }
+    console.log('toggleChat()');
+  }
+
+  onKeydown(event) {
+    if (this.text.length > 0) {
+      this.addMessage();
+      this.text = '';
+    }
+  }
+
+  addMessage() {
+    this.db.collection('messages').doc('msg-' + v1()).set({
+      'timestamp': + new Date(), // https://stackoverflow.com/questions/221294/how-do-you-get-a-timestamp-in-javascript
+      'text': this.text,
+      'author': this.authService.getName()
+    });
+  }
+
+  getMessage(messageId) {
+    this.messageDoc = this.db.doc('messages/' + messageId);
+    this.message = this.messageDoc.valueChanges();
+  }
+
+  initGlobalChat() {
+    this.channel = {
+        icon: {
+          prefix: 'fas',
+          name: 'globe'
+        },
+        name: 'global'
+      };
   }
 
 }
