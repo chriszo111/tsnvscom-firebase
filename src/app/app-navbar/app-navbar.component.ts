6f0371d83c4bca05cbc361e63a60c47c9789edda
chriszo111 <chrisswiss@gmx.net>
2019-01-06 00:17:49 +0100

    Display uid to user

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { AlertService } from '../services/alert.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './app-navbar.component.html',
   styleUrls: ['./app-navbar.component.css']
 })
-export class AppNavbarComponent implements OnInit {
+export class AppNavbarComponent {
 
-  constructor() { }
+  constructor(public authService: AuthService,
+              private alertService: AlertService) { }
 
-  ngOnInit() {
+  getUid() {
+    this.authService.getUid();
   }
-
 }
