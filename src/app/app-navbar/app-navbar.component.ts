7746d98cc2a01ce3913801de18bc3632c8912ade
chriszo111 <chrisswiss@gmx.net>
2019-01-07 22:37:15 +0100

    Inject router to navbar

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertService } from '../services/alert.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,8 @@ import { Observable } from 'rxjs';
 export class AppNavbarComponent {
 
   constructor(public authService: AuthService,
-              private alertService: AlertService) { }
+              private alertService: AlertService,
+              public router: Router) { }
 
   getUid() {
     this.authService.getUid();
