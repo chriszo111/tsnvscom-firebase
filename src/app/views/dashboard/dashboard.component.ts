49b616dad031761f325a698fb1013423481824ac
chriszo111 <chrisswiss@gmx.net>
2019-01-09 01:03:43 +0100

    Create new dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
+import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { AlertService } from 'src/app/services/alert.service';
 
 interface Badge {
   type: string;
@@ -18,12 +20,20 @@ export class DashboardComponent implements OnInit {
 
   displayName: Observable<string>;
   photoUrl: Observable<string>;
+  showEditPicture: boolean;
+  profilePhoto: string;
+  public files: UploadFile[] = [];
 
   constructor(public authService: AuthService,
+              private alertService: AlertService,
               private titleService: Title) { }
 
   ngOnInit() {
     this.titleService.setTitle('Dashboard - Tensation Virtual Services - tsnvs.com');
+    this.showEditPicture = false;
+    this.profilePhoto = '../../../assets/img/dummy-user.jpg';
+
+    console.log(this.authService.userData);
   }
 
   getEmailVerified(): Badge {
@@ -38,4 +48,62 @@ export class DashboardComponent implements OnInit {
     this.authService.sendVerificationEmail();
   }
 
+  toggleEditPicture() {
+    if (this.showEditPicture) {
+      this.profilePhoto = '../../../assets/img/dummy-user.jpg';
+      this.showEditPicture = false;
+    } else {
+      this.profilePhoto = '../../../assets/img/edit-profile-photo.png';
+      this.showEditPicture = true;
+    }
+  }
+
+  public dropped(event: UploadEvent) {
+    this.files = event.files;
+    for (const droppedFile of event.files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          if (this.authService.userData.uid === null) {
+            this.alertService.triggerAlert('danger', 'Your user id could not be found.');
+          }
+
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event) {
+    console.log(event);
+  }
+
+  public fileLeave(event) {
+    console.log(event);
+  }
 }
