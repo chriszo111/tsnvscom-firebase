08c869cd6abcfae92841b79cc646f6a89de3b281
chriszo111 <chrisswiss@gmx.net>
2019-09-07 18:09:19 +0200

    Cleanup node_modules, start using express with firebase

--- backend/app.routes.js
+++ backend/app.routes.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({isActive: true, status: 'online'})
+  res.status(200).json({isActive: true, status: 'online'})
 })
+
+/**
+ * Users
+ * GET /users/:id
+ * POST /users/create
+ * PUT /users/update
+ * DELETE /users/delete
+ */
 
 module.exports = router;
